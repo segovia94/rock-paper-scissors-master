082a3e6fd16831ba8225f173a9189f0d4e8798b3
Mark Miller <mark@zhukmedia.com>
2020-04-21 02:38:07 -0700

    mobile layout

--- js/example.js
+++
@@ -1,6 +0,0 @@
-// Require a library like jQuery.
-// import jQuery from 'jquery';
-
-(() => {
-  console.log('ES6 Converted with Babel');
-})();
---
+++ js/script.js
@@ -0,0 +1,13 @@
+(() => {
+  const modal = document.querySelector('.modal')
+  const modalClose = document.querySelector('.modal__close')
+  const btnRules = document.querySelector('[data-rules]')
+
+  btnRules.addEventListener('click', () => {
+    modal.classList.add('modal--open')
+  })
+
+  modalClose.addEventListener('click', (event) => {
+    modal.classList.remove('modal--open')
+  })
+})();
